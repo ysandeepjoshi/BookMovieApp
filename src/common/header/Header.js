27b1b1c25653fa9c191faa00507637a6689fe618
Sandeep Joshi <ysandeepjoshi@gmail.com>
2021-11-22 13:57:45 +0530

    rotating the logo

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -4,7 +4,7 @@ import logo from '../../assets/logo.svg';
  const Header = (props) =>{
     return (
           <div className="header-custom">
-              <span><img className="logo" src={logo} alt="logo was here"></img></span></div>
+              <span><img className="logo image-rotate" src={logo} alt="logo was here"></img></span></div>
     )
 }
 export default Header;
